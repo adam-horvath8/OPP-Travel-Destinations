cb1f8e30ba929a690be31e3e96b00ed85807eba6
adam-horvath8 <adam.horvath16299@gmail.comm>
2023-06-29 17:45:30 +0200

    Add popup mesagge

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-const inputName = document.getElementById("name");
-const inputCountry = document.getElementById("country");
-const inputPrice = document.getElementById("price");
-const submitButton = document.getElementById("btn");
-const body = document.querySelector("body");
-
-function TravelDestination(name, country, price) {
+function getTravelDestination(name, country, price) {
   return {
     name,
     country,
@@ -12,13 +6,54 @@ function TravelDestination(name, country, price) {
   };
 }
 
+function getEmptyInput() {
+  if (inputName.value.trim() === "") {
+    return "You forgot to enter Name";
+  } else if (inputCountry.value.trim() === "") {
+    return "You forgot to enter Country";
+  } else if (inputPrice.value.trim() === "") {
+    return "You forgot to enter Price";
+  }
+}
+function getPopUpMessage(message) {
+  if (!document.getElementById("alert-div-id")) {
+    const alertDiv = document.createElement("div");
+    alertDiv.id = "alert-div-id";
+    alertDiv.classList.add("alert-div");
+    alertDiv.innerText = message;
+    titleDiv.appendChild(alertDiv);
+
+    const duration = 2000;
+    showAndHideDiv(alertDiv, duration);
+
+    function showAndHideDiv(element, duration) {
+      element.style.display = "flex";
+
+      setTimeout(function () {
+        element.remove();
+      }, duration);
+    }
+  }
+}
+
+// Query Selectors
+const inputName = document.getElementById("name");
+const inputCountry = document.getElementById("country");
+const inputPrice = document.getElementById("price");
+const tableDiv = document.getElementById("table-div");
+const submitButton = document.getElementById("btn");
+const body = document.querySelector("body");
+const titleDiv = document.getElementById("title-div");
+
+
+
 const table = document.createElement("table");
 if (!document.getElementById("table-id")) {
   table.id = "table-id";
   table.classList.add("destination-table");
 
   const tableHeaderRow = document.createElement("tr");
-  // tableHeaderRow.classList.add("table-header-row");
+  tableHeaderRow.classList.add("table-header-row");
   const tableNameHeader = document.createElement("th");
   tableNameHeader.innerText = "Name";
   const tableCountryHeader = document.createElement("th");
@@ -30,17 +65,23 @@ if (!document.getElementById("table-id")) {
   table.append(tableHeaderRow);
 }
 
+// Submit Button
 submitButton.addEventListener("click", function makeTable() {
-  const destination = TravelDestination(
+  // Create object
+  const destination = getTravelDestination(
     inputName.value,
     inputCountry.value,
     inputPrice.value
   );
+
+  // Add limit to rows created
   const destinationTableRows =
     document.getElementsByClassName("destination-row");
   if (destinationTableRows.length > 8) {
-    alert("You can enter oply up to 7 destinations");
-  } else {
+    getPopUpMessage("You can only add up to 7 destinations");
+  }
+  // Create destination rows
+  else {
     if (
       inputName.value.trim() !== "" &&
       inputCountry.value.trim() !== "" &&
@@ -49,13 +90,17 @@ submitButton.addEventListener("click", function makeTable() {
       const destinationTableRow = document.createElement("tr");
       destinationTableRow.className += "destination-row";
       destinationTableRow.classList.add("ligth-row");
+
       const nameTableData = document.createElement("td");
       nameTableData.innerText = destination.name;
+
       const countryTableData = document.createElement("td");
       countryTableData.innerText = destination.country;
+
       const priceTableData = document.createElement("td");
       priceTableData.innerText = destination.price + " $";
       priceTableData.classList.add("priceDataCell");
+
       const deleteButton = document.createElement("i");
       deleteButton.innerHTML = "<i class='fa-solid fa-circle-xmark'></i>";
 
@@ -72,20 +117,12 @@ submitButton.addEventListener("click", function makeTable() {
         priceTableData
       );
       priceTableData.appendChild(deleteButton);
-      table.append(destinationTableRow);
-      body.appendChild(table);
-    } else {
-      alert(getEmptyInput());
+      table.appendChild(destinationTableRow);
+      tableDiv.appendChild(table);
+      body.appendChild(tableDiv);
+    }
+     else {
+      getPopUpMessage(getEmptyInput());
     }
   }
 });
-
-function getEmptyInput() {
-  if (inputName.value.trim() === "") {
-    return "You forgot to enter Name";
-  } else if (inputCountry.value.trim() === "") {
-    return "You forgot to enter Country";
-  } else if (inputPrice.value.trim() === "") {
-    return "You forgot to enter Price";
-  }
-}
